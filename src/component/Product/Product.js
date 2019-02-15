d09295ae0b245591d43638c7f597f51f819963ca
unknown <zoltan.berente@outlook.com>
2019-02-15 16:49:33 -0700

    slight styling

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../../App.css'
 
 const Product = props => {
     return (
-        <div key={props.index}>
-            <div><img src={props.product.img_url} alt='Product Img'></img></div>
+        <div key={props.index} className='product'>
+            <div><img src={props.product.img_url} alt='Product Img' className='product_img'></img></div>
             <h1>{props.product.product_name}</h1>
             <h3>${props.product.price}</h3>
             <button>Edit</button>
